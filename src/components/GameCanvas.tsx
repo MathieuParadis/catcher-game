15e9051723d4d9dd8de6d575116688aa8d87d999
MathieuParadis <mathieuyl.paradis@gmail.com>
2024-03-23 23:07:34 +0800

    refactor into component and changed ratio

---
+++ src/components/GameCanvas.tsx
@@ -0,0 +1,8 @@
+// REACT IMPORT
+import React from 'react'
+
+const GameCanvas = (): JSX.Element => {
+  return <div className="square-class flex justify-center items-center bg-black"></div>
+}
+
+export default GameCanvas
