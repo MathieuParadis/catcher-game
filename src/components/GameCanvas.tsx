4cd165855542a60df3f8177e442dd8a4bf66b1e5
MathieuParadis <mathieuyl.paradis@gmail.com>
2024-03-23 23:13:58 +0800

    refactor

--- src/components/GameCanvas.tsx
+++ src/components/GameCanvas.tsx
@@ -2,7 +2,7 @@
 import React from 'react'
 
 const GameCanvas = (): JSX.Element => {
-  return <div className="square-class flex justify-center items-center bg-black"></div>
+  return <div className="responsive-canvas flex justify-center items-center bg-black"></div>
 }
 
 export default GameCanvas
