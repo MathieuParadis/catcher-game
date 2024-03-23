a0f9eb78fcaee262ab188a56ccaa8eb4f4c42c05
MathieuParadis <mathieuyl.paradis@gmail.com>
2024-03-23 22:59:30 +0800

    made the play area

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,9 @@ import React from 'react'
 
 function App(): JSX.Element {
   return (
-    <div className="h-screen w-screen flex justify-center items-center bg-red-300 overflow-hidden"></div>
+    <div className="h-screen w-screen flex justify-center items-center bg-red-300 p-4 md:p-10 overflow-hidden">
+      <div className="square-class flex justify-center items-center bg-black"></div>
+    </div>
   )
 }
 
