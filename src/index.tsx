4f43c2b81b839bf22f0c793e4517fbcaff6e0d1b
MathieuParadis <mathieuyl.paradis@gmail.com>
2024-03-23 16:41:46 +0800

    added eslint

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// REACT IMPORT
+import React from 'react'
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+// REACT-DOM IMPORT
+import ReactDOM from 'react-dom/client'
+
+// APP IMPORT
+import App from './App'
+
+// STYLES IMPORTS
+import './index.css'
+
+const rootElement = document.getElementById('root')
+if (rootElement == null) throw new Error('Failed to find the root element')
+
+const root = ReactDOM.createRoot(rootElement)
+
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+)
