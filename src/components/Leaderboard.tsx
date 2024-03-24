40c6f5e1a9492adb1e41fa0995c954c63d35ea66
MathieuParadis <76945713+MathieuParadis@users.noreply.github.com>
2024-03-24 10:53:37 +0800

    Feat/screen mode (#5)
    * added assets
    
    * created 2 boards
    
    * linted

---
+++ src/components/Leaderboard.tsx
@@ -0,0 +1,10 @@
+// REACT IMPORT
+import React from 'react'
+
+const Leaderboard = (): JSX.Element => {
+  return (
+    <div className="absolute top-0 left-0 h-full w-full bg-[url('../assets/image/bg2.png')] bg-cover"></div>
+  )
+}
+
+export default Leaderboard
