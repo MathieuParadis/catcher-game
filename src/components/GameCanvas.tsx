d5e989cd7bdcbc12000d377af674710c032aef1f
MathieuParadis <mathieuyl.paradis@gmail.com>
2024-03-24 10:52:05 +0800

    created 2 boards

--- src/components/GameCanvas.tsx
+++ src/components/GameCanvas.tsx
@@ -1,8 +1,22 @@
 // REACT IMPORT
 import React from 'react'
 
+// COMPONENTS IMPORTS
+import Playboard from './Playboard'
+import Leaderboard from './Leaderboard'
+
 const GameCanvas = (): JSX.Element => {
-  return <div className="responsive-canvas flex justify-center items-center bg-black"></div>
+  return (
+    <div className="relative responsive-canvas flex justify-center items-center bg-[#448ee9]">
+      <div className="flex flex-col justify-center items-center gap-2">
+        <button className="p-2 w-[150px] border">Play</button>
+        <button className="p-2 w-[150px] border whitespace-nowrap">See ranking</button>
+      </div>
+
+      <Playboard />
+      <Leaderboard />
+    </div>
+  )
 }
 
 export default GameCanvas
---
+++ src/components/Playboard.tsx
@@ -0,0 +1,11 @@
+// REACT IMPORT
+import React from 'react'
+
+const Playboard = (): JSX.Element => {
+  return (
+    <div className="absolute top-0 left-0 h-full w-full bg-[url('../assets/image/bg1.png')] bg-cover">
+    </div>
+  )
+}
+
+export default Playboard
