f933812d9e44d627bf59ec355ee6c96a22b0ed17
MathieuParadis <mathieuyl.paradis@gmail.com>
2024-03-24 11:06:20 +0800

    Added data and types

---
+++ src/data/ranking.ts
@@ -0,0 +1,20 @@
+// TYPES IMPORTS
+import { RankRecordType } from '../types/rankRecordType'
+
+export const ranking: RankRecordType[] = [
+  {
+    id: 0,
+    score: 200,
+    date: new Date()
+  },
+  {
+    id: 1,
+    score: 300,
+    date: new Date()
+  },
+  {
+    id: 2,
+    score: 230,
+    date: new Date()
+  }
+]
---
+++ src/types/rankRecordType.ts
@@ -0,0 +1,6 @@
+export interface RankRecordType {
+    id: string | number
+    score: number
+    date: Date
+  }
+  
