4cc870f1e3a0a170271156ffe4f5d32ff07abbfe
MathieuParadis <mathieuyl.paradis@gmail.com>
2024-03-24 12:13:04 +0800

    edited leadboard

--- src/components/Leaderboard.tsx
+++ src/components/Leaderboard.tsx
@@ -5,7 +5,18 @@ const Leaderboard = (): JSX.Element => {
   return (
     <div className="absolute top-0 left-0 h-full w-full bg-[url('../assets/image/bg2.png')] bg-cover">
       <div className="h-full w-full p-6">
-        <div className="h-full w-full bg-white opacity-50">rules here</div>
+        <div className="relative h-full w-full">
+          {/* overlay */}
+          <div className="absolute top-0 left-0 h-full w-full bg-white opacity-30"></div>
+
+          {/* content */}
+          <div className="absolute top-0 left-0 h-full w-full overflow-auto flex flex-col items-center">
+            <h1 className="text-6xl text-green-400 mb-5">Ranking</h1>
+            <p className="text-white mb-8">
+              Ready to see where you stand? Check out the top players below.
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   )
