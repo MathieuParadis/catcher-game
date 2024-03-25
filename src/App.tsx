8a307bc52e1394641af51a1f9c24a42d8fba3aa9
MathieuParadis <76945713+MathieuParadis@users.noreply.github.com>
2024-03-25 21:46:03 +0800

    UI/bg color (#13)
    * bg color of app
    
    * linted

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,41 @@
-// REACT IMPORT
-import React from 'react'
+// REACT IMPORTS
+import React, { useEffect, useState } from 'react'
 
 // COMPONENTS IMPORTS
 import Game from './components/Game'
 
 function App(): JSX.Element {
+  const [bgColor, setBgColor] = useState('#000000')
+
+  useEffect(() => {
+    const updateTimeBasedBackgroundColor = (): void => {
+      const currentTime = new Date().getHours()
+      let newBgColor = ''
+
+      if (currentTime >= 6 && currentTime < 9) {
+        newBgColor = '#ccf3ff'
+      } else if (currentTime >= 9 && currentTime < 18) {
+        newBgColor = '#00bfff'
+      } else if (currentTime >= 18 && currentTime < 21) {
+        newBgColor = '#6388d4'
+      } else {
+        newBgColor = '#020035'
+      }
+      setBgColor(newBgColor)
+    }
+
+    updateTimeBasedBackgroundColor()
+    const interval = setInterval(updateTimeBasedBackgroundColor, 60 * 1000)
+
+    return () => {
+      clearInterval(interval)
+    }
+  }, [])
+
   return (
-    <div className="h-screen w-screen flex justify-center items-center bg-red-300 p-4 md:p-10 overflow-hidden">
+    <div
+      className="h-screen w-screen flex justify-center items-center p-4 md:p-10 overflow-hidden"
+      style={{ backgroundColor: bgColor }}>
       <Game />
     </div>
   )
