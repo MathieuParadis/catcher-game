ef6f0532fcf12c9d7c5b46d2fe844151ddd49a89
MathieuParadis <mathieuyl.paradis@gmail.com>
2024-03-25 16:01:27 +0800

    displayed boat on convas

--- src/components/Playboard/PlayCanvas.tsx
+++ src/components/Playboard/PlayCanvas.tsx
@@ -1,5 +1,5 @@
 // REACT IMPORTS
-import React, { useRef } from 'react'
+import React, { useEffect, useRef } from 'react'
 
 // COMPONENTS
 import Timer from './Timer'
@@ -11,6 +11,8 @@ import {
   handleTurnOffStartTimer,
   handleStopGame
 } from '../../redux/slices/playModeSlice'
+
+import boat from '../../assets/image/boat.png'
 
 const PlayCanvas = (): JSX.Element => {
   const dispatch = useAppDispatch()
@@ -26,15 +28,71 @@ const PlayCanvas = (): JSX.Element => {
     dispatch(handleStopGame())
   }
 
+  useEffect(() => {
+    if (canvasRef.current != null) {
+      const ctx = canvasRef.current.getContext('2d')
+      if (ctx != null) {
+        // Calculate the width of the boat (20% of canvas width)
+        const canvasWidth = canvasRef.current.width
+        const canvasHeight = canvasRef.current.height
+        const rectangleWidth = canvasWidth * 0.2
+
+        // Calculate the position to draw the rectangle in the middle
+        const rectangleX = (canvasWidth - rectangleWidth) / 2
+        const rectangleY = canvasHeight - 10
+
+        // Draw the rectangle
+        ctx.fillStyle = 'blue' // Set fill color
+        ctx.fillRect(rectangleX, rectangleY, 50, 1) // Draw the rectangle
+      }
+    }
+  }, [canvasRef, isStartTimerActive, isGameOver])
+
+  useEffect(() => {
+    if (canvasRef.current != null) {
+      const ctx = canvasRef.current.getContext('2d')
+
+      // Adjust canvas size for Retina displays
+      const scaleFactor = window.devicePixelRatio
+      canvasRef.current.width = canvasRef.current.clientWidth * scaleFactor
+      canvasRef.current.height = canvasRef.current.clientHeight * scaleFactor
+
+      if (ctx != null) {
+        const img = new Image()
+        img.src = boat
+
+        // Calculate the width of the boat (20% of canvas width)
+        const canvasWidth = canvasRef.current.width
+        const canvasHeight = canvasRef.current.height
+        const boatWidth = canvasWidth * 0.2
+        const boatHeight = boatWidth * 1.23
+
+        // Calculate the position to draw the boat in the middle
+        const boatX = (canvasWidth - boatWidth) / 2
+        const boatY = canvasHeight - boatHeight - 50
+
+        // Draw the boat
+        img.onload = () => {
+          ctx.drawImage(img, boatX, boatY, boatWidth, boatHeight)
+        }
+      }
+    }
+  }, [canvasRef, isStartTimerActive, isGameOver])
+
   return (
     <>
+      {/* Start timer */}
       {isStartTimerActive && <Timer countdownSeconds={3} onExpire={turnOffStartTimer} />}
+
+      {/* Game is active */}
       {!isStartTimerActive && (
         <div className="relative w-full h-full flex justify-center items-center">
           {!isGameOver && (
             <>
-              <button className="absolute top-0 right-0 p-2 w-[150px] border z-[10]">Pause</button>
-              <Timer countdownSeconds={60} onExpire={stopGame} />
+              <div className="absolute top-0 right-0 flex gap-8">
+                <Timer countdownSeconds={60} onExpire={stopGame} />
+                <button className="p-2 w-[150px] border z-[10]">Pause</button>
+              </div>
               <canvas className="w-full h-full" ref={canvasRef}></canvas>
             </>
           )}
