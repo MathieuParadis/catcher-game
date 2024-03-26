75836a689a37a2ea9dc933e7a00c3bcc194e7102
MathieuParadis <76945713+MathieuParadis@users.noreply.github.com>
2024-03-26 16:34:23 +0800

    UI/ranking (#16)

--- src/components/Leaderboard/Leaderboard.tsx
+++ src/components/Leaderboard/Leaderboard.tsx
@@ -17,21 +17,27 @@ const Leaderboard = (): JSX.Element => {
 
   return (
     <div className="absolute top-0 left-0 h-full w-full bg-[url('../assets/image/bg2.png')] bg-cover">
-      <div className="h-full w-full p-6">
+      <div className="h-full w-full">
         <div className="relative h-full w-full">
           {/* overlay */}
           <div className="absolute top-0 left-0 h-full w-full bg-gray-700 opacity-90"></div>
-          <button className="absolute p-2 w-[150px] border z-[10]" onClick={backToMainMenu}>
+          <button
+            className="absolute z-10 top-2 md:top-4 lg:top-6 left-2 md:left-4 lg:left-6 font1 p-1 md:p-2 pt-2 md:pt-4 w-[70px] md:w-[100px] lg:w-[110px] aspect-[379/200] text-white text-base md:text-lg lg:text-xl bg-[url('../assets/image/woodboard.png')] bg-cover hover:scale-110"
+            onClick={backToMainMenu}>
             Back
           </button>
 
           {/* content */}
-          <div className="absolute top-0 left-0 h-full w-full overflow-auto flex flex-col items-center p-4">
-            <h1 className="text-6xl text-green-400 mb-5">Ranking</h1>
-            <p className="text-2xl text-white text-center mb-8">
-              Ready to see where you stand?<br></br>Check out the top players below.
-            </p>
-            <RankingTable />
+          <div className="absolute top-0 left-0 h-full w-full flex flex-col items-center p-2 md:p-4 lg:p-6 overflow-hidden">
+            <h1 className="text-4xl md:text-5xl lg:text-6xl text-yellow-500 font1 my-2 md:my-4 drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
+              Ranking
+            </h1>
+            <div className="w-full md:flex md:flex-col md: justify-center md:items-center overflow-auto no-scrollbar pt-1">
+              <p className="text-xl md:text-2xl lg:text-3xl font2 text-white text-center mb-8">
+                Ready to see where you stand?<br></br>Check out the top players below.
+              </p>
+              <RankingTable />
+            </div>
           </div>
         </div>
       </div>
--- src/components/Leaderboard/RankingTable.tsx
+++ src/components/Leaderboard/RankingTable.tsx
@@ -1,55 +1,38 @@
-import * as React from 'react'
-import {
-  Paper,
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TableRow,
-  Typography
-} from '@mui/material'
+// REACT IMPORT
+import React from 'react'
+
+// TYPES IMPORTS
 import type { RankRecordType } from '../../types/rankRecordTypes'
+
+// DATA IMPORTS
 import { ranking } from '../../data/ranking'
 
 const RankingTable = (): JSX.Element => {
   const sortedRanking = ranking.sort((a, b) => b.score - a.score)
 
   return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead className="sticky top-0 z-10">
-          <TableRow className="border border-b bg-red-200">
-            <TableCell align="right">
-              <Typography variant="h6" fontWeight="bold">
-                Rank
-              </Typography>
-            </TableCell>
-            <TableCell align="right">
-              <Typography variant="h6" fontWeight="bold">
-                Score
-              </Typography>
-            </TableCell>
-            <TableCell align="right">
-              <Typography variant="h6" fontWeight="bold">
-                Player
-              </Typography>
-            </TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {sortedRanking.map(
-            (rank: RankRecordType, index: number): JSX.Element => (
-              <TableRow key={rank.id}>
-                <TableCell align="right">{index + 1}</TableCell>
-                <TableCell align="right">{rank.score}</TableCell>
-                <TableCell align="right">{rank.playerName}</TableCell>
-              </TableRow>
-            )
-          )}
-        </TableBody>
-      </Table>
-    </TableContainer>
+    <table className="min-w-full sm:min-w-[550px]">
+      <thead className="bg-sky-800 text-left text-white py-3 sticky top-[-4px]">
+        <tr>
+          <th className="py-3 text-center w-[80px]">Rank</th>
+          <th className="py-3 pl-2">Player</th>
+          <th className="py-3 pl-2">Score</th>
+        </tr>
+      </thead>
+      <tbody className="text-black bg-white overflow-y-scroll">
+        {sortedRanking.map(
+          (rank: RankRecordType, index: number): JSX.Element => (
+            <tr
+              key={index}
+              className="odd:bg-white even:bg-blue-100 hover:bg-cyan-600 hover:text-white">
+              <td className="border-y border-[#ddd] py-2 text-center">{index + 1}</td>
+              <td className="border-y border-[#ddd] py-2 pl-2">{rank.playerName}</td>
+              <td className="border-y border-[#ddd] py-2 pl-2">{rank.score}</td>
+            </tr>
+          )
+        )}
+      </tbody>
+    </table>
   )
 }
 
