91c18cde80e5b3643d520c4ea16eb2464ac1af7f
MathieuParadis <mathieuyl.paradis@gmail.com>
2024-03-26 14:14:47 +0800

    Edited buttons

--- src/components/Playboard/Rules.tsx
+++ src/components/Playboard/Rules.tsx
@@ -19,11 +19,8 @@ const Rules = (): JSX.Element => {
       <div className="absolute top-0 left-0 h-full w-full overflow-auto flex flex-col items-center p-4">
         <h1 className="text-6xl text-green-400 mb-5">Rules</h1>
         <p className="text-2xl text-white text-center mb-8">Rules here</p>
-        <button className="p-2 w-[150px] border bg-red-800" onClick={startGame}>
-          ok start
-        </button>
         <button
-          className="md:top-4 left-2 md:left-4 font1 p-1 md:p-2 w-[120px] md:w-[160px] lg:w-[200px] aspect-[379/200] text-white text-xl md:text-2xl lg:text-3xl bg-[url('../assets/image/woodboard.png')] bg-cover hover:scale-110"
+          className="font1 p-1 md:p-2 pt-2 md:pt-4 w-[80px] md:w-[110px] lg:w-[120px] aspect-[379/200] text-white text-xl md:text-2xl lg:text-3xl bg-[url('../assets/image/woodboard.png')] bg-cover hover:scale-110"
           onClick={startGame}>
           Start
         </button>
