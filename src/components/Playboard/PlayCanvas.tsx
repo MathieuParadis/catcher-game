51798d3d2be8e2c0eed362b626d52df5012731fc
MathieuParadis <mathieuyl.paradis@gmail.com>
2024-03-27 08:48:00 +0800

    improving readability

--- src/components/Playboard/PlayCanvas.tsx
+++ src/components/Playboard/PlayCanvas.tsx
@@ -128,35 +128,33 @@ const PlayCanvas = (): JSX.Element => {
       )}
 
       {/* Game is active */}
-      {!isStartTimerActive && (
+      {!isStartTimerActive && !isGameOver && (
         <div className="relative w-full h-full flex justify-center items-center">
-          {!isGameOver && (
-            <>
-              <div className="absolute top-0 right-0 flex gap-8">
-                <Timer countdownSeconds={60} onExpire={stopGame} />
-                <button className="p-2 w-[150px] border z-[10]">Pause</button>
-              </div>
-              <button
-                className="absolute z-10 top-2 md:top-4 lg:top-6 left-2 md:left-4 lg:left-6 text-2xl md:text-3xl lg:text-4xl font1 w-[70px] md:w-[100px] lg:w-[110px] aspect-[379/200] text-white bg-[url('../assets/image/woodboard.png')] bg-cover hover:scale-110"
-                onClick={turnMusicOnOff}>
-                {isMusicOn ? (
-                  <VolumeUpOutlinedIcon fontSize="inherit" />
-                ) : (
-                  <VolumeOffOutlinedIcon fontSize="inherit" />
-                )}
-              </button>
-              <canvas
-                className="w-full h-full p-0 m-0"
-                ref={canvasRef}
-                onMouseMove={handleMouseMove}></canvas>
-              <audio className="hidden" ref={audioRef} muted={!isMusicOn}>
-                <source src={music} type="audio/mpeg" />
-              </audio>
-            </>
-          )}
-
-          {isGameOver && <>game over</>}
+          <div className="absolute top-0 right-0 flex gap-8">
+            <Timer countdownSeconds={60} onExpire={stopGame} />
+            <button className="p-2 w-[150px] border z-[10]">Pause</button>
+          </div>
+          <button
+            className="absolute z-10 top-2 md:top-4 lg:top-6 left-2 md:left-4 lg:left-6 text-2xl md:text-3xl lg:text-4xl font1 w-[70px] md:w-[100px] lg:w-[110px] aspect-[379/200] text-white bg-[url('../assets/image/woodboard.png')] bg-cover hover:scale-110"
+            onClick={turnMusicOnOff}>
+            {isMusicOn ? (
+              <VolumeUpOutlinedIcon fontSize="inherit" />
+            ) : (
+              <VolumeOffOutlinedIcon fontSize="inherit" />
+            )}
+          </button>
+          <canvas
+            className="w-full h-full p-0 m-0"
+            ref={canvasRef}
+            onMouseMove={handleMouseMove}></canvas>
+          <audio className="hidden" ref={audioRef} muted={!isMusicOn}>
+            <source src={music} type="audio/mpeg" />
+          </audio>
         </div>
+      )}
+
+      {!isStartTimerActive && isGameOver && (
+        <div className="relative w-full h-full flex justify-center items-center">game over</div>
       )}
     </>
   )
--- src/components/Playboard/Playboard.tsx
+++ src/components/Playboard/Playboard.tsx
@@ -16,7 +16,7 @@ import { selectPlayModeState } from '../../redux/slices/playModeSlice'
 const Playboard = (): JSX.Element => {
   const dispatch = useAppDispatch()
   const playMode = useAppSelector(selectPlayModeState)
-  const { isDisplayRules } = playMode
+  const { areRulesDisplayed } = playMode
 
   const backToMainMenu = (): void => {
     dispatch(handleBackToMainMenu())
@@ -26,7 +26,7 @@ const Playboard = (): JSX.Element => {
     <div className="absolute top-0 left-0 h-full w-full bg-[url('../assets/image/bg1.png')] bg-cover">
       <div className="relative h-full w-full">
         {/* rules */}
-        {isDisplayRules && (
+        {areRulesDisplayed && (
           <>
             <button
               className="absolute z-10 top-2 md:top-4 lg:top-6 left-2 md:left-4 lg:left-6 text-2xl md:text-3xl lg:text-4xl font1 w-[70px] md:w-[100px] lg:w-[110px] aspect-[379/200] text-white bg-[url('../assets/image/woodboard.png')] bg-cover hover:scale-110"
@@ -38,7 +38,7 @@ const Playboard = (): JSX.Element => {
         )}
 
         {/* game */}
-        {!isDisplayRules && <PlayCanvas />}
+        {!areRulesDisplayed && <PlayCanvas />}
       </div>
     </div>
   )
