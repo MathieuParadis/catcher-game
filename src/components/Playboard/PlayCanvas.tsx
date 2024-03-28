f316d3c27fb01543ccb0322016f8630acb4c3782
MathieuParadis <mathieuyl.paradis@gmail.com>
2024-03-28 20:24:38 +0800

    fix and refactor

--- src/components/Playboard/PlayCanvas.tsx
+++ src/components/Playboard/PlayCanvas.tsx
@@ -60,8 +60,8 @@ const PlayCanvas = (): JSX.Element => {
   const [tempBoatX, setTempBoatX] = useState(0)
   const [score, setScore] = useState(0)
   const [tempMusicCurrentTime, setTempMusicCurrentTime] = useState(0)
-  const [item, setItem] = useState<ItemWithPositionType>()
-  // const [items, setItems] = useState<ItemWithPositionType[]>([])
+  // const [item, setItem] = useState<ItemWithPositionType[]>([])
+  const [items, setItems] = useState<ItemWithPositionType[]>([])
 
   const turnOffStartTimer = (): void => {
     dispatch(handleTurnOffStartTimer())
@@ -119,8 +119,9 @@ const PlayCanvas = (): JSX.Element => {
   }
 
   const generateRandomItem = (): ItemWithPositionType => {
+    const randomIndex = random(0, imgItems.length - 1, false)
     return {
-      ...imgItems[random(0, imgItems.length, false)],
+      ...imgItems[randomIndex],
       id: '',
       x: 0,
       y: 0,
@@ -178,76 +179,87 @@ const PlayCanvas = (): JSX.Element => {
   ])
 
   useEffect(() => {
-    setItem(generateRandomItem())
-
-    // const initialItems = Array.from({ length: 5 }, () => generateRandomItem())
-    // setItems(initialItems)
+    const initialItems = Array.from({ length: 5 }, () => generateRandomItem())
+    setItems(initialItems)
   }, [])
 
   // Draw items on canvas
   useEffect(() => {
-    if (canvasRef.current != null && item != null) {
-      const ctx = canvasRef.current.getContext('2d')
-      const animation = animationRef.current
+    items.forEach((ite) => {
+      if (canvasRef.current != null && ite != null) {
+        const ctx = canvasRef.current.getContext('2d')
+        const animation = animationRef.current
 
-      const animate = (): void => {
-        if (canvasRef.current != null && ctx != null && animation != null) {
-          const img = new Image()
-          img.src = item.img
+        const animate = (): void => {
+          if (canvasRef.current != null && ctx != null && animation != null) {
+            const img = new Image()
+            img.src = ite.img
 
-          // Calculate the width of the item (12.5% of canvas width)
-          const canvasWidth = canvasRef.current.width
-          const canvasHeight = canvasRef.current.height
-          const itemWidth = canvasWidth * 0.125
-          const itemHeight = itemWidth * 1
+            // Calculate the width of the item (12.5% of canvas width)
+            const canvasWidth = canvasRef.current.width
+            const canvasHeight = canvasRef.current.height
+            const itemWidth = canvasWidth * 0.125
+            const itemHeight = itemWidth * 1
 
-          // Random initial positon of the item
-          const initialItemX = random(0, canvasWidth - itemWidth)
-          const initialItemY = 0 - itemHeight
+            // Random initial positon of the item
+            const initialItemX = random(0, canvasWidth - itemWidth)
+            const initialItemY = 0 - itemHeight
 
-          // Draw the item
-          img.onload = () => {
-            if (isStartResumeTimerActive && !isGameInProgress && item.x === 0 && item.y === 0) {
-              setItem({ ...item, x: initialItemX, y: initialItemY, w: itemWidth, h: itemHeight })
-            } else if (!isStartResumeTimerActive && isGameInProgress && !isGamePaused) {
-              setItem({ ...item, y: item.y + item.speed, w: itemWidth, h: itemHeight })
-            } else {
-              setItem({ ...item, w: itemWidth, h: itemHeight })
-            }
+            // Draw the item
+            img.onload = () => {
+              if (isStartResumeTimerActive && !isGameInProgress && ite.x === 0 && ite.y === 0) {
+                setItems([
+                  { ...ite, x: initialItemX, y: initialItemY, w: itemWidth, h: itemHeight }
+                ])
+              } else if (!isStartResumeTimerActive && isGameInProgress && !isGamePaused) {
+                setItems([
+                  {
+                    ...ite,
+                    y: ite.y + ite.speed,
+                    w: itemWidth,
+                    h: itemHeight
+                  }
+                ])
+              } else {
+                setItems([{ ...ite, w: itemWidth, h: itemHeight }])
+              }
 
-            ctx.clearRect(item.x, item.y, itemWidth, itemHeight)
-            ctx.drawImage(img, item.x, item.y, itemWidth, itemHeight)
+              ctx.clearRect(ite.x, ite.y, itemWidth, itemHeight)
+              ctx.drawImage(img, ite.x, ite.y, itemWidth, itemHeight)
 
-            // Check for collision
-            if (
-              checkCatch({
-                obj1: boat,
-                obj2: item
-              })
-            ) {
-              // clear item on canvas and sending it out
-              ctx.clearRect(item.x, item.y, itemWidth, itemHeight)
-              setItem({
-                ...item,
-                x: -canvasWidth,
-                y: canvasHeight
-              })
-              setScore(item.value)
+              // Check for collision
+              if (
+                checkCatch({
+                  obj1: boat,
+                  obj2: ite
+                })
+              ) {
+                // clear item on canvas and sending it out
+                ctx.clearRect(ite.x, ite.y, itemWidth, itemHeight)
+                setItems([
+                  {
+                    ...ite,
+                    x: -canvasWidth,
+                    y: canvasHeight
+                  }
+                ])
+                setScore(ite.value)
+              }
             }
           }
         }
-      }
 
-      // Start animation loop
-      animate()
+        // Start animation loop
+        animate()
 
-      // Clean up function
-      return () => {
-        if (animation != null) {
-          cancelAnimationFrame(animation)
+        // Clean up function
+        return () => {
+          if (animation != null) {
+            cancelAnimationFrame(animation)
+          }
         }
       }
-    }
+    })
   }, [
     canvasRef,
     isStartResumeTimerActive,
@@ -256,7 +268,7 @@ const PlayCanvas = (): JSX.Element => {
     boat,
     tempBoatX,
     score,
-    item
+    items
   ])
 
   // control of the audio
