06c7ddd2e46bcbd7de054a90eca5035e58f19a35
MathieuParadis <mathieuyl.paradis@gmail.com>
2024-03-29 10:00:34 +0800

    Edited game over view

--- src/components/Playboard/GameOver.tsx
+++ src/components/Playboard/GameOver.tsx
@@ -1,5 +1,5 @@
-// REACT IMPORT
-import React from 'react'
+// REACT IMPORTS
+import React, { useState } from 'react'
 
 // REDUX IMPORTS
 import { useAppDispatch } from '../../redux/hooks'
@@ -15,6 +15,9 @@ interface Props {
 
 const GameOver = ({ score }: Props): JSX.Element => {
   const dispatch = useAppDispatch()
+  const [isScoreSubmitted, setIsScoreSubmitted] = useState(false)
+
+  console.log(setIsScoreSubmitted)
 
   const backToMainMenu = (): void => {
     dispatch(handleBackToMainMenu())
@@ -31,25 +34,34 @@ const GameOver = ({ score }: Props): JSX.Element => {
       <div className="absolute top-0 left-0 h-full w-full bg-gray-700 opacity-90"></div>
 
       {/* Content */}
-      <div className="absolute top-0 left-0 h-full w-full overflow-auto flex flex-col justify-center items-center p-2 md:p-4 lg:p-6">
-        <h1 className="text-5xl md:text-7xl lg:text-9xl text-center text-yellow-500 font1 my-2 md:my-4 drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
+      <div className="absolute top-0 left-0 h-full w-full overflow-auto flex flex-col justify-center items-center gap-10 md:gap-16 lg:gap-24  p-2 md:p-4 lg:p-6">
+        <h1 className="text-5xl md:text-7xl lg:text-9xl text-center text-yellow-500 font1 my-2 md:my-4 lg:my-6 drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
           Game Over
         </h1>
-        <p className="text-3xl font2 text-white text-center mb-8">Congratulations!</p>
-        <p className="text-3xl font2 text-white text-center mb-8">Your score: {score}</p>
 
-        <div className="flex justify-center items-center font1 mt-10 sm:mt-14 md:m-0 gap-6 md:gap-7 lg:gap-8">
-          <button
-            className="p-1 md:p-2 w-[120px] md:w-[160px] lg:w-[200px] aspect-[379/200] text-white text-xl md:text-2xl lg:text-3xl bg-[url('../assets/image/woodboard.png')] bg-cover hover:scale-110"
-            onClick={playAgain}>
-            Play<br></br>again
-          </button>
-          <button
-            className="p-1 md:p-2 w-[120px] md:w-[160px] lg:w-[200px] aspect-[379/200] text-white text-xl md:text-2xl lg:text-3xl bg-[url('../assets/image/woodboard.png')] bg-cover hover:scale-110"
-            onClick={backToMainMenu}>
-            Main<br></br>menu
-          </button>
-        </div>
+        {/* Form */}
+        {isScoreSubmitted && (
+          <>
+            <p className="text-3xl font2 text-white text-center mb-8">Congratulations!</p>
+            <p className="text-3xl font2 text-white text-center mb-8">Your score: {score}</p>
+          </>
+        )}
+
+        {/* Buttons */}
+        {!isScoreSubmitted && (
+          <div className="flex justify-center items-center font1 gap-6 md:gap-8 lg:gap-12">
+            <button
+              className="p-1 md:p-2 w-[120px] md:w-[160px] lg:w-[200px] aspect-[379/200] text-white text-xl md:text-2xl lg:text-3xl bg-[url('../assets/image/woodboard.png')] bg-cover hover:scale-110"
+              onClick={playAgain}>
+              Play<br></br>again
+            </button>
+            <button
+              className="p-1 md:p-2 w-[120px] md:w-[160px] lg:w-[200px] aspect-[379/200] text-white text-xl md:text-2xl lg:text-3xl bg-[url('../assets/image/woodboard.png')] bg-cover hover:scale-110"
+              onClick={backToMainMenu}>
+              Main<br></br>menu
+            </button>
+          </div>
+        )}
       </div>
     </>
   )
